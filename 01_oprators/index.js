ae43e703dd122d4c103945b84d860f4df437b8c9
Omkar Daptare <154222712+omkar-98@users.noreply.github.com>
2024-10-23 18:28:59 +0000

    Operators And String files add

---
+++ 01_oprators/index.js
@@ -0,0 +1,9 @@
+console.log(4/5);
+
+let a = 5**5;
+console.log(a);
+
+
+let b = 45;
+let c = "45";
+console.log("b===c is",b===c);
---
+++ 02_Strings/index.js
@@ -0,0 +1,25 @@
+let name  = "Omkar Bhai";
+console.log(name.length);
+
+let friend = "Pavan";
+
+console.log(`${name } is friend of the ${friend}`);
+
+console.log(`${friend} is the friend of ${name}`);
+
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(2,7));
+console.log(name.slice(2));
+console.log(name.replace("Bhai","Bhau\n"));
+console.log(name.concat(name,"Daptare"));
+console.log(name.concat(" daptare"));
+console.log(name.trim());
+
+
+
+
+for(let i = 0; i<name.length ;i++){
+    console.log(name[i]);
+}
